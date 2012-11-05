0d9a9157b95d5b4f86207f39517a818ff72418f3
isidroo <drago_013@hotmail.com>
2012-11-05 04:59:13 +0000

    Se automatiza el agregar elementos en la Jtable

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,22 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class Principal {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -35,8 +46,106 @@ public class Principal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 700, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 658, Short.MAX_VALUE)
+						.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 658, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(78, Short.MAX_VALUE))
+		);
+		
+		JButton btnGeneraElementos = new JButton("Genera Elementos");
+		btnGeneraElementos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				Object obj[][] = {
+						{1, "Isidro", "Martínez", edadRamd()},
+						{2, "Abelardo", "López", edadRamd()},
+						{3, "Karen", "Quintero", edadRamd()},
+						{4, "Jaime", "Rodriguez", edadRamd()},
+						{5, "Roberto", "De la O", edadRamd()},
+						{6, "Maria", "Antúnez", edadRamd()},
+						{7, "Ana", "Ramírez", edadRamd()},
+						{8, "Luz", "Vargas", edadRamd()},
+						{9, "Diego", "Cervantes", edadRamd()},
+						{10, "Laura", "Rios", edadRamd()},
+						{11, "Leonardo", "Villa Nueva", edadRamd()},
+						{12, "Omar", "Zaragoza", edadRamd()},
+						{13, "Fernando", "Mendoza", edadRamd()},
+						{14, "Diana", "Flores", edadRamd()},
+						{15, "Luis", "Soler", edadRamd()}
+					};
+				for(int i=0;i<15;i++){
+					((DefaultTableModel) table.getModel()).insertRow(i,obj[i]);
+				}
+			}
+		});
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnGeneraElementos)
+					.addContainerGap(549, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnGeneraElementos)
+					.addContainerGap(33, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"ID", "NOMBRE", "APELLIDOS", "EDAD"
+			}
+		));
+		scrollPane.setViewportView(table);
+		frame.getContentPane().setLayout(groupLayout);
 	}
+	
+	private int edadRamd(){
+		int entero;
+		entero = (int) Math.round(Math.random()*15);
 
+		return entero;
+	}
 }
